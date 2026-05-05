943876c5ab0eaf8eab2d068d04a36b0328deaa09
Kevin Xiao <dt_xiaoqijun@qq.com>
2026-05-05 16:10:49 +0800

    chore: remove temp files and unused vite template assets

--- frontend_vue3/_start_vite.mjs
+++
@@ -1,9 +0,0 @@
-import('vite').then(({ createServer }) => {
-  createServer({
-    root: '.',
-    configFile: './vite.config.js',
-    server: { port: 8080, host: true }
-  }).then(server => {
-    server.listen()
-  })
-})
--- frontend_vue3/src/router/index.js
+++ frontend_vue3/src/router/index.js
@@ -1,15 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { useUserStore } from '../stores/user'
 
-const demoRoutes = [
-  {
-    path: 'view-switch-demo',
-    name: 'ViewSwitchDemo',
-    component: () => import('../views/demo/ViewSwitchDemo.vue'),
-    meta: { title: '视图切换对比' }
-  }
-]
-
 const routes = [
   {
     path: '/login',
@@ -47,7 +38,7 @@ const routes = [
         component: () => import('../views/RolePermission.vue'),
         meta: { title: '角色权限', permission: 'role-view' }
       },
-      ...demoRoutes
+
     ]
   }
 ]
